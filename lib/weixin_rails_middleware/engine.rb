54f76aa25307db554f1b1428998ae649e6be388f
lanrion <huaitao-deng@foxmail.com>
2014-02-24 23:23:40 +0800

    added engine_name

--- lib/weixin_rails_middleware/engine.rb
+++ lib/weixin_rails_middleware/engine.rb
@@ -1,5 +1,8 @@
 module WeixinRailsMiddleware
   class Engine < ::Rails::Engine
     isolate_namespace WeixinRailsMiddleware
+    # rake weixin_rails_middleware:install:migrations
+    engine_name :weixin_rails_middleware
+
   end
 end
