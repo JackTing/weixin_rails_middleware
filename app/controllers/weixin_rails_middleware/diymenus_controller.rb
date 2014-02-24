762209ecd3f9187821c17c19674a26fc5597a31b
lanrion <huaitao-deng@foxmail.com>
2014-02-24 23:25:59 +0800

    updated diymenus controller

--- app/controllers/weixin_rails_middleware/diymenus_controller.rb
+++ app/controllers/weixin_rails_middleware/diymenus_controller.rb
@@ -1,8 +1,60 @@
 module WeixinRailsMiddleware
   class DiymenusController < ApplicationController
+    before_action :set_diymenu, only: [:show, :edit, :update, :destroy]
 
+    # GET /diymenus
     def index
-
+      @diymenus = Diymenu.all
     end
+
+    # GET /diymenus/1
+    def show
+    end
+
+    # GET /diymenus/new
+    def new
+      @diymenu = Diymenu.new
+    end
+
+    # GET /diymenus/1/edit
+    def edit
+    end
+
+    # POST /diymenus
+    def create
+      @diymenu = Diymenu.new(diymenu_params)
+
+      if @diymenu.save
+        redirect_to @diymenu, notice: 'Diymenu was successfully created.'
+      else
+        render action: 'new'
+      end
+    end
+
+    # PATCH/PUT /diymenus/1
+    def update
+      if @diymenu.update(diymenu_params)
+        redirect_to @diymenu, notice: 'Diymenu was successfully updated.'
+      else
+        render action: 'edit'
+      end
+    end
+
+    # DELETE /diymenus/1
+    def destroy
+      @diymenu.destroy
+      redirect_to diymenus_url, notice: 'Diymenu was successfully destroyed.'
+    end
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_diymenu
+        @diymenu = Diymenu.find(params[:id])
+      end
+
+      # Only allow a trusted parameter "white list" through.
+      def diymenu_params
+        params.require(:diymenu).permit(:public_account_id, :parent_id, :title, :keyword, :is_show, :sort, :url)
+      end
   end
 end
